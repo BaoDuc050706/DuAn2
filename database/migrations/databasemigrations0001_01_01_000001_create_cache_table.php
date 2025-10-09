19354ca8c669a4209e0b68f997d046fa6dc79569
BaoDuc050706 <baoduc050706@gmail.com>
2025-10-09 16:48:55 +0700

    database fixed

--- database/migrations/databasemigrations0001_01_01_000001_create_cache_table.php
+++ database/migrations/databasemigrations0001_01_01_000001_create_cache_table.php
@@ -9,15 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-        public function up(): void
+    public function up(): void
     {
         Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
             $table->integer('expiration');
         });
-    }
 
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration');
+        });
+    }
 
     /**
      * Reverse the migrations.
@@ -25,5 +30,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_locks');
     }
 };
