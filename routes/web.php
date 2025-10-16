b1e647836fdfea1f5d33b4e0eb10a9f19ac0320c
Anezus <quocanhnguyen1707@gmail.com>
2025-10-16 16:57:00 +0700

    ggg

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
-
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ProfileController;
